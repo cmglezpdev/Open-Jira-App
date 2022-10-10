fd0f736c4a665e02b35a2cd397d8c95e71f9f46f
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-10 10:47:29 -0400

    feat(Root): add data of the api in the provider

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,9 +1,9 @@
-import { FC, ReactNode, useReducer } from "react";
+import { FC, ReactNode, useEffect, useReducer } from "react";
 import { v4 as uuid } from 'uuid';
 
 import { entriesReducer, EntriesContext } from "./";
-import { Entry, EntryStatus } from '../../interfaces';
-
+import { Entry } from '../../interfaces';
+import { entriesApi } from "../../apis";
 
 export interface EntriesState {
     entries: Entry[];
@@ -34,6 +34,15 @@ export const EntriesProvider: FC<{ children: ReactNode}> = ({ children }) => {
     const updateEntry = ( entry: Entry ) => {
         dispatch({type: '[Entry] Update Entry', payload: entry});
     }
+
+    const refreshEntries = async () => {
+        const { data } = await entriesApi.get<Entry[]>('/entries');
+        dispatch({type: '[Entry] Refresh Data', payload: data});
+    }
+
+    useEffect(() => {
+        refreshEntries();
+    }, [])
 
     return (
         <EntriesContext.Provider value={{
--- context/entries/entriesReducer.ts
+++ context/entries/entriesReducer.ts
@@ -5,6 +5,7 @@ import { Entry } from '../../interfaces/entry';
 type EntriesActionType = 
     | { type: '[Entry] AddEntry', payload:Entry }
     | { type: '[Entry] Update Entry', payload:Entry }
+    | { type: '[Entry] Refresh Data', payload:Entry[] }
 
 
 
@@ -20,6 +21,12 @@ export const entriesReducer = ( state: EntriesState, action:EntriesActionType ) 
             return {
                 ...state,
                 entries: state.entries.map( entry => ( entry._id === action.payload._id ) ? action.payload : entry )
+            }
+        
+        case '[Entry] Refresh Data':
+            return {
+                ...state,
+                entries: [...action.payload]
             }
         default:
             return state;
