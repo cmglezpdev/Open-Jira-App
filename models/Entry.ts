694613f523d1a44d26f6de9bf6b374d3e082ea16
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-10 10:32:31 -0400

    feat(Root): add get entries endpoint in my restfull API

--- models/Entry.ts
+++ models/Entry.ts
@@ -1,7 +1,7 @@
 import mongoose, { Model, Schema } from "mongoose";
 import { Entry } from "../interfaces";
 
-interface IEntry extends Entry {}
+export interface IEntry extends Entry {}
 
 const entrySchema = new Schema({
     description: {
